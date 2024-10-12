e15dcde0af8894438d49fe942056da3ce53c3c5d
SergeyP-GA <140579701+SergeyP-GA@users.noreply.github.com>
2024-10-12 19:24:41 +0800

    Update 006-gcc-stage2.sh

--- scripts/006-gcc-stage2.sh
+++ scripts/006-gcc-stage2.sh
@@ -73,8 +73,8 @@ for TARGET in "mips64r5900el-ps2-elf"; do
     --with-sysroot="$PS2DEV/$TARGET_ALIAS/$TARGET" \
     --with-native-system-header-dir="/include" \
 #    --with-newlib \
-    --disable-libssp \
-    --disable-multilib \
+#    --disable-libssp \
+#    --disable-multilib \
     --disable-nls \
     --disable-tls \
     --enable-cxx-flags=-G0 \
